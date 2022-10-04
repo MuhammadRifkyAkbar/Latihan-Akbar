f1c69692ec5f34365f486a1fbbf129e459e31066
Muhammad Rifky Akbar <114640734+MuhammadRifkyAkbar@users.noreply.github.com>
2022-10-04 13:44:36 +0700

    Upload File josh

---
+++ josh.cpp
@@ -0,0 +1,23 @@
+// Tempat untuk file header
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+
+using namespace std;
+
+int main () {
+    int sialannn = 57;
+    cout << "umur saya adalahh " << sialannn <<endl;
+
+    sialannn = 40;
+    cout << "umur saya adalahh " << sialannn <<endl;
+
+    const double AJH = 6.89;
+    cout << "nilai AJH adalah " << AJH;
+
+    return 0;
+
+
+}
